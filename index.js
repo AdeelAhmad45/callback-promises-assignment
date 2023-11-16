9169554b33daaad3482ebd72d8045188b7e1f5cf
Adeel Ahmad <129967189+AdeelAhmad45@users.noreply.github.com>
2023-11-16 11:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,15 +22,15 @@ const doubledNum = (arr, cb) => {
     return `The manipulated string is: ${logStringResult}`;
  };
  
- console.log(manipulateString("Vishal", logString));
+ console.log(manipulateString("Addy", logString));
  
  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  
  // Q.3 Write a JavaScript function called ageInDays that accepts an object containing a person's first name, last name, and age in years as input. The function should concatenate the first and last name into a single string and store it in a variable called fullName. It should then calculate the person's age in days and store it in a variable called ageInDays.The ageInDays function should then return a callback function that logs a message to the console. The message should include the person's full name and age in days, and should be in the format: "The person's full name is [full name] and their age in days is [age in days]." Note that the ageInDays function should not log the message to the console directly, but should instead returna callback function that can be used to log the message at a later time.
  
  let obj = {
-    firstName: "Vishal",
-    lastName: "Dubey",
+    firstName: "Addy",
+    lastName: "Ahmad",
     years: 1996,
  };
  const ageInDays = (person, cb) => {
@@ -139,7 +139,7 @@ const doubledNum = (arr, cb) => {
        }
     });
  }
- greet("Vishal")
+ greet("Addy")
     .then((message) => console.log(message))
     .catch((error) => console.log(error));
  
